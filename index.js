db7f82527ac92e8c1a9cb0204bad6816fe4b7378
jiwon79 <19-079@ksa.hs.kr>
2021-09-01 12:27:55 +0900

    fix: Add heroku setting

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const express = require('express') //express를 설치했기 때문에 가져올 수 있다.
 const app = express()
+const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
-app.listen(5000)
+app.listen(port, function(){
+  console.log('server on! http://localhost:'+port);
+});
