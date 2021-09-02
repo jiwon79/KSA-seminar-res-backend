04f164888b1c687a08a9cd143fa0c9871405cc7f
jiwon79 <19-079@ksa.hs.kr>
2021-09-03 02:31:28 +0900

    feat: Add connection mongo db

--- index.js
+++ index.js
@@ -2,8 +2,45 @@ const express = require('express') //express를 설치했기 때문에 가져올
 const app = express()
 const port = process.env.PORT || 3000;
 
+var mongoose = require('mongoose');
+mongoose.connect(process.env.KSA_SEMINAR_KIOSK_DB);
+var db = mongoose.connection;
+db.on('error', function(){
+    console.log('Connection Failed!');
+});
+db.once('open', function() {
+    console.log('Connected!');
+});
+
+var Schema = mongoose.Schema;
+var userLogSchema = new Schema(
+  {
+    name: String,
+    time: String,
+    number: String
+  }
+)
+// var userLogSchema = new Schema(
+//   {
+//     currentTime: String, 
+//     stu_num: String, 
+//     stu_name: String, 
+//     phone_num: String,
+//     option: String,
+//     info: {
+//       seminar: String,
+//       time: String
+//     }
+//   }
+// )
+
+
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('hello world');
+})
+
+app.get('/auth', (req, res) => {
+  res.send(req.query.id+','+req.query.pw)
 })
 
 app.listen(port, function(){
